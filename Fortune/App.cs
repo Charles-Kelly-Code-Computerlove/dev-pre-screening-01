aa94473803a60b4a91d46e459f940f512c1da18d
Charles Kelly <charles.kelly@codecomputerlove.com>
2020-09-07 15:35:10 +0100

    refactor: extracted some methods in app class to make it easier to what's happening

--- Fortune/App.cs
+++ Fortune/App.cs
@@ -16,22 +16,51 @@ namespace Fortune
 
 		public void Run()
 		{
-			_console.WriteLine("What's your name? ");
-			var name = _console.ReadLine();
+			var name = AskForAndGetName();
 
-			_console.WriteLine("When were you born (dd/mm/yyyy)?");
-			DateTime dateTime;
-			if (!DateTime.TryParseExact(_console.ReadLine(), "dd/mm/yyyy", new CultureInfo("GB"), DateTimeStyles.None,
-				out dateTime))
+			try
+			{
+				AskForAndGetDateOfBirth();
+			}
+			catch
 			{
 				_console.WriteLine("That's not a date of birth. No fortune for you!");
 				return;
 			}
 
+			GiveFortune(name);
+		}
+
+		private DateTime AskForAndGetDateOfBirth()
+		{
+			_console.WriteLine("When were you born (dd/mm/yyyy)?");
+			var dateOfBirthInput = _console.ReadLine();
+
+			var dateOfBirthFormat = "dd/mm/yyyy";
+
+			if (!DateTime.TryParseExact(dateOfBirthInput, dateOfBirthFormat, new CultureInfo("GB"),
+				DateTimeStyles.None,
+				out var dateOfBirth))
+			{
+				throw new Exception("Input from user is not in specified date format");
+			}
+
+			return dateOfBirth;
+		}
+
+		private void GiveFortune(string name)
+		{
 			var person = new Person {Name = name};
 
 			_console.WriteLine(
 				$"Hi {person.Name}!\nYour fortune for today is: {_fortuneCookie.GetTodaysFortune()}");
+		}
+
+		private string AskForAndGetName()
+		{
+			_console.WriteLine("What's your name? ");
+			var name = _console.ReadLine();
+			return name;
 		}
 	}
 }
